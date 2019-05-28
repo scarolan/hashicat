b82226f07eb0b49e0800f88fef8093203e06fac1
Ada Lovelace <ada@example.com>
2019-05-28 19:29:43 +0000

    Add container app

--- main.tf
+++ main.tf
@@ -3,18 +3,18 @@ resource "azurerm_resource_group" "myresourcegroup" {
   location = "${var.location}"
 }
 
-# module "web_app_container" {
-#   source              = "app.terraform.io/seanc-sandbox/web-app-container/azurerm"
-#   name                = "${var.prefix}"
-#   port                = "80"
-#   https_only          = "false"
-#   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
-#   container_type      = "docker"
-#   container_image     = "scarolan/palacearcade"
-# }
-# output "container_app_url" {
-#   value = "http://${module.web_app_container.hostname}"
-# }
+module "web_app_container" {
+  source              = "app.terraform.io/seanc-sandbox/web-app-container/azurerm"
+  name                = "${var.prefix}"
+  port                = "80"
+  https_only          = "false"
+  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+  container_type      = "docker"
+  container_image     = "scarolan/palacearcade"
+}
+output "container_app_url" {
+  value = "http://${module.web_app_container.hostname}"
+}
 
 # resource "azurerm_virtual_network" "vnet" {
 #   name                = "${var.prefix}-vnet"
