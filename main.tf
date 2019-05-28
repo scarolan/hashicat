8c4a257a3a514c2b3cbc2bb9a037aedaffff4a6e
Sean Carolan <scarolan@hashicorp.com>
2019-05-28 14:58:32 -0500

    enforce https

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "web_app_container" {
   source              = "app.terraform.io/seanc-sandbox/web-app-container/azurerm"
   name                = "${var.prefix}"
   port                = "80"
-  https_only          = "false"
+  https_only          = "true"
   resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
   container_type      = "docker"
   container_image     = "scarolan/pizzaplanet"
