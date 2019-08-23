3c62364322d6832a9ba3d562af00c6e1ac7fcf7a
Sean Carolan <scarolan@hashicorp.com>
2019-08-23 14:26:31 -0500

    Dogs >> Cats

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Woof Woof!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Hello world! I'm in your computer! 
+  <center><h2>Woof World!</h2></center>
+  Welcome to ${PREFIX}'s app. This is dog.
   <!-- END -->
   
   </div>
