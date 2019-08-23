8161bae3308d1caf68666948f622dd35a1fff197
Ada Lovelace <ada@lovelace.com>
2019-08-23 17:45:03 +0000

    Update remote backend setting.

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "yourname-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat"
     }
   }
 }
